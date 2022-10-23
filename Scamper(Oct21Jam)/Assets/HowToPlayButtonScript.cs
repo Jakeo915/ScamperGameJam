7c4c2470e633ec8162f4650cfe813f09ee1e25cd
Gwalker1313 <111939848+Gwalker1313@users.noreply.github.com>
2022-10-23 16:37:15 -0600

    Menu
    Added Main Menu

---
+++ Scamper(Oct21Jam)/Assets/HowToPlayButtonScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HowToPlayButtonScript : MonoBehaviour
+{
+    public void HowToPlayGame()
+    {
+        // Loads into image that explains how to play the game.
+        SceneManager.LoadScene(2);
+    }
+}
